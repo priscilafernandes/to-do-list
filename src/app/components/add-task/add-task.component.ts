8dcedc54a90d44b5bd44e93d4de7b760efb277cc
priscilafernandes <pris1@outlook.com.br>
2020-04-23 19:49:45 -0300

    event click works

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -9,16 +9,32 @@ import { BroadcastTodoService } from './../broadcast-todo.service'
 })
 export class AddTaskComponent implements OnInit {
 
-  //@Output() sendTask = new EventEmitter<any>();
-  
+  todoInput: string
+  placeholder: string = "Estudar, terminar projeto..."
+
   constructor(private taskService: TaskService, private _link: BroadcastTodoService) { }
-  
+
   ngOnInit(): void {
   }
-  
+
+  changeTodo(event) {
+    this.todoInput = event.target.value;
+  }
 
   sendTodoData(todo:string) {
-    this._link.passTodoValue(todo);
+    if (todo != '') {
+      this._link.passTodoValue(todo);
+    } else {
+      this.placeholder = "O campo não pode estar vazio!"
+    }
+  }
+
+  sendTodoDataClick() {
+    if (this.todoInput == undefined || this.todoInput.length <= 0) {
+      this.placeholder = "O campo não pode estar vazio!"
+    } else {
+      this._link.passTodoValue(this.todoInput);
+    }
   }
 
 }
