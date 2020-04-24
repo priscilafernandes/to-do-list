03673ff5499a19b935ff9a8f839d33a3ff75dd2d
priscilafernandes <pris1@outlook.com.br>
2020-04-23 23:30:18 -0300

    justify grid cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatIconModule} from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,6 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
 import { MainViewComponent } from './components/main-view/main-view.component';
 import { NavBarComponent } from './components/template/menu/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
 import { TaskCardsComponent } from './components/task-cards/task-cards.component';
 import { FormsModule } from '@angular/forms';
 
